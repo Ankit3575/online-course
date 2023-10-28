8edd83f93621d4d3bf524478e64085ac1b0c6b1f
Ankit3575 <royalankit254@gmail.com>
2023-10-28 16:30:17 +0530

    some problem fix

--- index.php
+++ index.php
@@ -11,104 +11,99 @@
 
 <body>
     <div class="container-fluid">
-<<<<<<< HEAD
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Onilne Course</a>
 
-        <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark">
-            <div class="container-fluid ">
-                <a class="navbar-brand" href="#">ONLINE COURSE</a>
-=======
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Onilne Course</a>
->>>>>>> d58999c83050b005cdf3c18490db9c8e140975cc
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Courses
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                                 </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Courses
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Action</a></li>
+                                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        <li>
+                                            <hr class="dropdown-divider">
+                                        </li>
+                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="">Services</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="">Contact</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="">Signup</a>
+                                </li>
+
                             </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="">Services</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="">Contact</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="">Signup</a>
-                        </li>
+                            <form class="d-flex" role="search">
+                                <input class="form-control border border-white me-2" type="search" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </div>
+                </nav>
+            </div>
 
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control border border-white me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+            <div class="container-fluid slidethumb" style="position: relative;">
+                <div class="row">
+                    <video loop="loop" autoplay playsinline muted controls>
+                        <source src="image/thumbenail.mp4" type="video/mp4">
+                    </video>
                 </div>
-            </div>
-        </nav>
-    </div>
-        <div class="container-fluid slidethumb" style="position: relative;">
-            <div class="row">
-                <video loop="loop" autoplay playsinline muted controls>
-                    <source src="image/thumbenail.mp4" type="video/mp4">
-                </video>
-            </div>
-            <div class="row">
-                <div class="col-sm-8"></div>
-                <div class="col-sm-4 ragister me-3">
-                    <form action="" method="post">
-                        <h4 class="align-items-center text-center p-3 fs-5 ">Register today and take your
-                            academics to the next level!</h4>
-                        <div class="mb-3 opacity-75 ">
-                            <label for="exampleInputName" class="form-label"></label>
-                            <input type="name" class="form-control border border-white" id="exampleInputName" aria-describedby="emailHelp" placeholder="Enter Your Name">
-                        </div>
-                        <div class="mb-3 opacity-75 ">
-                            <label for="exampleInputEmail1" class="form-label"></label>
-                            <input type="email" class="form-control border border-white" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your Email">
-                        </div>
-                        <div class="mb-3 opacity-75 ">
-                            <label for="exampleInputEmail1" class="form-label"></label>
-                            <input type="email" class="form-control border border-white" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your Contact">
-                        </div>
-                        <div class="mb-3 opacity-75 ">
-                            <label for="exampleInputEmail1" class="form-label"></label>
-                            <input type="email" class="form-control border border-white" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your Course">
-                        </div>
-                        <div class="mb-3 opacity-75 ">
-                        <label for="exampleInputEmail1" class="form-label"></label>
-                            <input type="email" class="form-control border border-white" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your Course">
-                        </div>
+                <div class="row">
+                    <div class="col-sm-8"></div>
+                    <div class="col-sm-4 ragister me-3">
+                        <form action="" method="post">
+                            <h4 class="align-items-center text-center p-3 fs-5 ">Register today and take your
+                                academics to the next level!</h4>
+                            <div class="mb-3 opacity-75 ">
+                                <label for="exampleInputName" class="form-label"></label>
+                                <input type="name" class="form-control border border-white" id="exampleInputName" aria-describedby="emailHelp" placeholder="Enter Your Name">
+                            </div>
+                            <div class="mb-3 opacity-75 ">
+                                <label for="exampleInputEmail1" class="form-label"></label>
+                                <input type="email" class="form-control border border-white" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your Email">
+                            </div>
+                            <div class="mb-3 opacity-75 ">
+                                <label for="exampleInputEmail1" class="form-label"></label>
+                                <input type="email" class="form-control border border-white" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your Contact">
+                            </div>
+                            <div class="mb-3 opacity-75 ">
+                                <label for="exampleInputEmail1" class="form-label"></label>
+                                <input type="email" class="form-control border border-white" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your Course">
+                            </div>
+                            <div class="mb-3 opacity-75 ">
+                                <label for="exampleInputEmail1" class="form-label"></label>
+                                <input type="email" class="form-control border border-white" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your Course">
+                            </div>
 
-                        <div class="mb-3 opacity-75">
-                        <label for="exampleInputButton1" class="form-label"></label>
-                            <input type="submit" class="form-control btn btn-warning  text-white border border-white" value="Request A CallBack">
-                        </div>
+                            <div class="mb-3 opacity-75">
+                                <label for="exampleInputButton1" class="form-label"></label>
+                                <input type="submit" class="form-control btn btn-warning  text-white border border-white" value="Request A CallBack">
+                            </div>
 
-                    </form>
+                        </form>
+                    </div>
+
                 </div>
 
             </div>
-
-        </div>
 
     </div>
 
